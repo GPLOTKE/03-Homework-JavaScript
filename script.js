82cb0b04626eab36ca7bd90202de3e9cdc6999c0
Jerry Plotke <jerry.plotke@gmail.com>
2021-09-09 12:29:45 -0500

    prompt choices function

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
+//ReadME Generator
+const writeme = require('writemereadme')
+writeme.init();
+
 // list of lowercase letters, uppercase letters, numbers, and special characters that can be used in password.
-// These are the values of the different variables
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['!', '"', '#', '$', '%', '&', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', '~'];
-    
-   // need a prompt function for letter/number/special character options in password and use if statements to reflect alerts if statement is true and if false the prompt will end.
-// we will need if statements to determine length of password.
-// the character variables will be used to confirm prompts.
+
+//function to prompt for length and type of characters
+function promptPasswordChoices() {
+    const length = prompt('Please enter your desired password length. Must be between 8 and 128 characters');
+
+    if (length > 128 || length < 8) {
+        alert("Password must be less than 8 and and no greater than 128 characters.")
+        return;
+    }
+
+    if (isNaN(length) === true) {
+        alert('Password length must be provided as a number');
+        return;
+    }
+
+    const hasLowerCase = confirm(
+        'Click OK to include lowercase characters.'
+    );
+
+    const hasUpperCase = confirm(
+        'Click OK to include uppercase characters.'
+    );
+
+    const hasNumbers = confirm(
+        'Click OK to include numbers.'
+    );
+
+    const hasSpecialCharacters = confirm(
+        'Click OK to confirm including special characters.'
+    );
+}
+
+//function to get random element
+
+//function to generate password
 
 
 // Assignment Code
-    var generateBtn = document.querySelector("#generate");
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
-    // Write password to the #password input
-    function writePassword() {
-      var password = generatePassword();
-      var passwordText = document.querySelector("#password");
-      passwordText.value = password;
-    }
-    
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
 
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
